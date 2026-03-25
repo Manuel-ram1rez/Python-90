0a07210def5d81b10a771b8451a10f345b2dc0c0
Manuel Ramírez <manuramirezcoche@gmail.com>
2026-03-25 13:59:44 -0600

    feat: Agregado de producto

--- ejercicio_json.py
+++ ejercicio_json.py
@@ -9,7 +9,48 @@ def leer_archivo_(presupuesto: int)-> list:
         print('No se pudo abrir el archivo, ERROR')
         return []
 
-resultado = leer_archivo_(200)
-#-----join() por estetica
-estetica = ', '.join(resultado)
-print(estetica)
+def agregar_producto():
+    ruta = 'tienda.json'
+    try:
+        with open (ruta, 'r', encoding='utf-8') as archivo:
+            inventario = json.load(archivo)
+        
+        nombre = input('Ingrese el nombre del producto: ')
+
+        try:
+            precio = int(input('Ingrese el precio del producto: '))
+
+        except ValueError:
+            print('tines que ingresar numeros')
+
+        con_stock = input('Hay producto si/no').lower()
+
+        if con_stock == 'si':
+            con_stock = True
+        elif con_stock == 'no':
+            con_stock = False
+
+        nuevo_item = {
+            'nombre': nombre,
+            'precio': precio,
+            'con_stock': con_stock
+        }
+
+        inventario.append(nuevo_item)
+        with open(ruta, 'w', encoding='utf-8') as archivo:
+            json.dump(inventario, archivo, indent=4)
+
+        print(f'{nombre} agregado correctamente')
+    except FileNotFoundError:
+        print('El archivo no existe en la BD')
+
+def ver_archivo():
+    with open ('tienda.json', 'r', encoding='utf-8') as archivo:
+        dato_tienda = json.load(archivo)
+        
+        for i in dato_tienda:
+            print(f'producto: {i['nombre']}: {i['precio']}  {i['con_stock']}')
+
+#agregar_producto()
+
+ver_archivo()
